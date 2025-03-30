823dcb491b8dfbfa7c1d9a71b878db5f2fa1c5d2
SutharHarsh <sutharharshp01@gmail.com>
2025-03-30 22:44:50 +0530

    Second Episode Done!! - Install node_modules - Install & Learn about Parcel - Install React and ReactDOM - Run React code using parcel bundler

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(heading)
+// console.log(heading)
 
 // root.render(heading);
 
-const Parent = React.createElement("div", { id: "parent" }, 
-    [React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "This is a h1 tag"),
-        React.createElement("h2", {}, "This is a h2 tag")
+const Parent = React.createElement("div", { id: "parent" , key: "parent-key" }, 
+    [React.createElement("div", { id: "child1" , key: "child1-key"}, [
+        React.createElement("h1", { id: "chile1_h1" , key: "h1-child1" }, "Hey, I am Harsh Suthar"),
+        React.createElement("h2", { id: "chile1_h2" , key: "h2-child1" }, "Computer Enginnering Student")
     ]),
-        React.createElement("div", { id: "child2" }, [
-            React.createElement("h1", {}, "This is a h1 tag"),
-            React.createElement("h2", {}, "This is a h2 tag")
+        React.createElement("div", { id: "child2" , key: "child2-key" }, [
+            React.createElement("h1", { id: "chile2_h1" , key: "h1-child2" }, "This is a h1 tag"),
+            React.createElement("h2", { id: "chile2_h2" , key: "h2-child2" }, "This is a h2 tag")
         ])
     ]
 
