15e9fdc1f1aaaa945564b60c4004f0f5a35a9144
SutharHarsh <sutharharshp01@gmail.com>
2025-04-01 22:01:56 +0530

    Learn below things: - Foundation - Basics of JSX

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!");
+
+const Title = () => (
+    <div>
+        <h1 className="head">Hello</h1>
+        <h2>My name is Harsh</h2>
+    </div>
+);
+
+const num = 1000;
+
+const HeadingComponent = () => (
+    <div id="container">
+        {num}
+        <Title />
+        {Title()}
+        <h1 id="heading"> Namste-React </h1>
+    </div>
+); 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// console.log(heading)
-
-// root.render(heading);
-
-const Parent = React.createElement("div", { id: "parent" , key: "parent-key" }, 
-    [React.createElement("div", { id: "child1" , key: "child1-key"}, [
-        React.createElement("h1", { id: "chile1_h1" , key: "h1-child1" }, "Hey, I am Harsh Suthar"),
-        React.createElement("h2", { id: "chile1_h2" , key: "h2-child1" }, "Computer Enginnering Student")
-    ]),
-        React.createElement("div", { id: "child2" , key: "child2-key" }, [
-            React.createElement("h1", { id: "chile2_h1" , key: "h1-child2" }, "This is a h1 tag"),
-            React.createElement("h2", { id: "chile2_h2" , key: "h2-child2" }, "This is a h2 tag")
-        ])
-    ]
-
-)
-
-root.render(Parent);
+root.render(<HeadingComponent />);
 
