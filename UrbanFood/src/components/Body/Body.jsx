283872a60e27b52294125a567c853127263ac123
SutharHarsh <sutharharshp01@gmail.com>
2025-06-15 11:35:22 +0530

    Implement Filter Function in UrbanFood

--- UrbanFood/src/components/Body/Body.jsx
+++ UrbanFood/src/components/Body/Body.jsx
@@ -1,16 +1,50 @@
 import ResCard from './ResCard'
 import resList from "../../utils/mockData"
+import { useState } from 'react'
 
 function Body() {
-    
+    const [rating, setRating] = useState(0)
+    const [filterApplied, setFilterApplied] = useState(false)
+    const [filterData, setFilterData] = useState([])
+
+    const filterRating = () => {
+        resList.map((filterRestaurant) => {
+            if (filterRestaurant.data.avgRating >= rating) {
+                setFilterData((prevData) => [...prevData, filterRestaurant])
+            }
+        })
+    }
+
+    const filterChange = () => {
+        filterRating()
+        setFilterApplied(() => !filterApplied)
+    }
+
     return (
         <div>
-            <div className='px-5 pt-5 flex gap-3'>
-                <input className='border-1 border-black' type="text" />
-                <button className='bg-gray-300 px-4 rounded-sm py-1 cursor-pointer'>Search</button>
+            <div className='flex gap-5 items-center'>
+                <div className='px-5 pt-5 flex gap-3'>
+                    <input className='border-1 border-black' type="text" />
+                    <button className='bg-gray-300 px-4 rounded-sm py-1 cursor-pointer'>Search</button>
+                </div>
+                <div className='flex items-center justify-center pt-5 gap-2'>
+                    <input
+                        onChange={(e) => setRating(e.target.value)}
+                        type="number"
+                        className='border-1 border-black py-1'
+                    />
+                    <button
+                        onClick={() => filterChange()}
+                        className='bg-amber-500 px-5 py-1 border-1 border-black cursor-pointer'
+                    >
+                        Filter</button>
+                </div>
             </div>
             <div className='flex flex-wrap'>
-                { resList.map((res) => <ResCard key={res.data.id} resData = {res} />) }
+                {
+                    filterApplied ? filterData.map((res) => <ResCard key={res.data.id} resData={res} />) :
+                        resList.map((res) => <ResCard key={res.data.id} resData={res} />)
+                }
             </div>
         </div>
     )
--- UrbanFood/src/utils/constants.js
+++ UrbanFood/src/utils/constants.js
@@ -1,3 +1,3 @@
-IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/5/22/6c8a7784-0b42-419e-81f9-f2ab2319a479_1003414.JPG"
+const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/5/22/6c8a7784-0b42-419e-81f9-f2ab2319a479_1003414.JPG"
 
 export default IMG_URL;
